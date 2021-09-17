707e5b59b675885c79c98d3831649136405460dc
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-09-16 15:29:13 -1000

    chore: add example schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,60 @@ module.exports = {
         accessToken:
           "MC5ZVVBkb2hJQUFDSUFneU1U.77-9c--_vUFE77-977-977-9Iu-_ve-_ve-_vUIBeF5yGu-_ve-_vRV8G--_vQbvv73vv73vv71-N--_ve-_vQ",
         schemas: {
-          page: require("./custom_types/page.json"),
+          page: {
+            Body: {
+              body: {
+                type: "Slices",
+                fieldset: "Slice zone",
+                config: {
+                  // labels: null,
+                  choices: {
+                    hero_feature: {
+                      type: "Slice",
+                      fieldset: "Hero Feature",
+                      description: "Intro to a feature page",
+                      icon: "burst_mode",
+                      display: "list",
+                      "non-repeat": {
+                        text: {
+                          type: "StructuredText",
+                          config: {
+                            multi:
+                              "paragraph,heading1,heading2,strong,em,hyperlink",
+                            allowTargetBlank: true,
+                            label: "Text",
+                          },
+                        },
+                        button_link: {
+                          type: "Link",
+                          config: {
+                            label: "Button Link",
+                            // select: null,
+                          },
+                        },
+                        button_text: {
+                          type: "StructuredText",
+                          config: {
+                            single: "paragraph",
+                            label: "Button Text",
+                          },
+                        },
+                        image: {
+                          type: "Image",
+                          config: {
+                            constraint: {},
+                            thumbnails: [],
+                            label: "Image",
+                          },
+                        },
+                      },
+                      repeat: {},
+                    },
+                  },
+                },
+              },
+            },
+          },
         },
       },
     },
